21ca03fd8504951324992e388487bac283a59ddf
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2024-03-02 16:34:19 +0000

    add basic seeds

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ class Item < ApplicationRecord
   has_many :item_shipping_methods
   has_many :shipping_methods , through: :item_shipping_methods
 
-  validates :name, :location, :description, :price, :condition, :quantity, :start_date, :end_date presence: true
+  validates :name, :location, :description, :price, :condition, :quantity, :start_date, :end_date, presence: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+p "create 10 users each user has 1 item"
+
+10.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip: Faker::Address.zip,
+    country: Faker::Address.country,
+    phone: Faker::PhoneNumber.cell_phone,
+    password: "123456"
+  )
+
+  user.save!
+  item = Item.create(
+    name: Faker::Commerce.product_name,
+    location: Faker::Address.city,
+    description: Faker::Lorem.paragraph,
+    quantity: Faker::Number.number(digits: 2),
+    price: Faker::Number.decimal(l_digits: 2),
+    condition: "good",
+    user: user,
+    start_date: Faker::Date.between(from: 50.days.ago, to: Date.today),
+    end_date: Faker::Date.between(from: Date.today, to: 50.days.from_now)
+  )
+  item.save!
+end
