8f0df6a9593f8a19273dd6cb284d0dba19a53d69
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2024-03-05 23:10:49 +0000

    placeholder card and index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
 end
