ce0b05ed4101c0e320d4c90a31f7a158669791a4
LauraNicolas <laura.cecilia.nicolas@gmail.com>
2024-03-07 00:47:09 +0100

    created carousel

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
+    @items = Item.all
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,12 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "stimulus-carousel" # @5.0.1
+pin "dom7" # @4.0.6
+pin "ssr-window" # @4.0.2
+pin "swiper/bundle", to: "swiper--bundle.js" # @8.4.7
