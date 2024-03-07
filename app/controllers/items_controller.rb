5b4c40a9d79d7437c59e6df0ea51535b495823c8
Joao Ribeiro <jprrib@gmail.com>
2024-03-07 19:35:47 +0000

    new and create items actions implemented

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,25 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    @item.save
+    redirect_to @item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :location, :quantity, :price, :condition, :description, :co2_emitions, images: [])
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   has_many :item_shipping_methods
   has_many :shipping_methods , through: :item_shipping_methods
 
+  has_many_attached :images
+
   validates :name, :location, :description, :price, :condition, :quantity, :start_date, :end_date, presence: true
 
 end
