c9752e2019773bea79ee5f6c402db461db3b41a1
Francesca Valsecchi <francesca_valsecchi@hotmail.com>
2024-03-08 19:27:34 +0100

    search bar added in the show page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,17 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.search(params[:search])
+  end
+
   def show
     @item = Item.find(params[:id])
     @items = Item.all
   end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :location, :description, :price, :condition, :quantity, :start_date, :end_date, :search)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
 
   validates :name, :location, :description, :price, :condition, :quantity, :start_date, :end_date, presence: true
 
+  def self.search(search)
+    if search
+      user_type = User.find_by(name: search)
+        if user_type
+          self.where(user_id: user_type)
+        else
+        @items = Item.all
+        end
+    else
+      @items = Item.all
+    end
+  end
 end
