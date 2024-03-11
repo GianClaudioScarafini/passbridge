89b7f8dc90cc72af23ad0499bf2152c78f9ab833
Francesca Valsecchi <francesca_valsecchi@hotmail.com>
2024-03-11 21:06:28 +0100

    adjusted files - delted duplicate coding

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,25 +1,8 @@
 class ItemsController < ApplicationController
   def index
-<<<<<<< HEAD
     @items = Item.search(params[:search])
-=======
-    @items = Item.all
->>>>>>> 969ea09bcb0f718f42df242a21606e0c72ec7ce4
   end
 
-  def show
-    @item = Item.find(params[:id])
-    @items = Item.all
-  end
-
-<<<<<<< HEAD
-private
-
-  def item_params
-    params.require(:item).permit(:name, :location, :description, :price, :condition, :quantity, :start_date, :end_date, :search)
-  end
-
-=======
   def new
     @item = Item.new
   end
@@ -32,12 +15,16 @@ private
     else
       render :new, status: :unprocessable_entity
     end
+
+    def show
+      @item = Item.find(params[:id])
+      @items = Item.all
+    end
   end
 
-  private
+private
 
   def item_params
-    params.require(:item).permit(:name, :location, :quantity, :price, :condition, :description, :co2_emitions, images: [])
+    params.require(:item).permit(:name, :location, :description, :price, :condition, :quantity, :start_date, :end_date, :search)
   end
->>>>>>> 969ea09bcb0f718f42df242a21606e0c72ec7ce4
 end
