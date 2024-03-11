6437a92bae5b824c78477233fdfeec442b420ba8
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2024-03-11 22:24:10 +0000

    add picture to index and zoom to all the

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -28,6 +28,6 @@ export default class extends Controller {
     #fitMapToMarkers() {
       const bounds = new mapboxgl.LngLatBounds()
       this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: .5 })
   }
 }
