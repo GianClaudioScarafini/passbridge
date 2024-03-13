9030bb4b1c722bddf8c7630287060b050d9feb52
GianClaudio Scarafini <g.claudio.scarafini@gmail.com>
2024-03-13 18:56:30 +0000

    edittime and carosels

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,9 +44,28 @@ class ItemsController < ApplicationController
     redirect_to items_path, status: :see_other
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if params[:item][:images].nil?
+      @item.update(item_params_no_imgs)
+    else
+      @item.update(item_params)
+    end
+    redirect_to item_path(@item)
+  end
+
+
   private
 
   def item_params
     params.require(:item).permit(:name, :location, :quantity, :price, :condition, :description, :co2_emitions, images: [])
+  end
+
+  def item_params_no_imgs
+    params.require(:item).permit(:name, :location, :quantity, :price, :condition, :description, :co2_emitions)
   end
 end
