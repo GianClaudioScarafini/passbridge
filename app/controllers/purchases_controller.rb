2a727dfbb42d11d1f5beec4592c8d580ae897820
Francesca Valsecchi <francesca_valsecchi@hotmail.com>
2024-03-18 22:39:39 +0100

    popup window added

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,11 @@
 class PurchasesController < ApplicationController
+  def create
+    @purchase = Purchase.new(purchase_params)
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:quantity, :other_attributes)
+  end
 end
